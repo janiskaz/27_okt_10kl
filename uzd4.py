442a336690ed88a561a9a1306baf2480fc9975a0
janiskaz <janikazo@madona.edu.lv>
2025-10-27 11:48:19 +0200

    izlabots uzd4.py
    norādīju kur bija kļūdas un izlaboju

--- uzd4.py
+++ uzd4.py
@@ -4,6 +4,7 @@ summma=0
 skaitlis=1
 
 while skaitlis<=100:
-    summma=skaitlis # summma=summma+skaitlis
-    skaitlis=1
+    summma+=skaitlis # summma=summma+skaitlis, kļūda1 jo nebija pielikts +
+    skaitlis+=1 # kļūda2 jo nebija pielikts +
+
 print("Summa no 1 līdz 100 ir:summma}")
