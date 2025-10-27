c93e0f74b1b62c32d99ecea4222a269aa31cfe27
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2025-10-27 10:40:24 +0200

    1.uzdevums
    Izlabot kļūdas un iesniegt darbu!

---
+++ uzd4.py
@@ -0,0 +1,9 @@
+# Summēt skaitļus no 1 līdz 100.
+
+summma=0
+skaitlis=1
+
+while skaitlis<=100:
+    summma=skaitlis # summma=summma+skaitlis
+    skaitlis=1
+print("Summa no 1 līdz 100 ir:summma}")
